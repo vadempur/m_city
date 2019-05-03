c426ab63d1ec33b23425beb293ebf778a32270ac
vadempur <shashi@techpool.co.in>
2019-05-04 01:16:04 +0530

    animation for intro page added

--- src/Components/home/featured/index.js
+++ src/Components/home/featured/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Striper from './Striper';
+import Text from './Text';
 
 const Featured = () => {
     return (
         <div className='featured_wrapper'>
           <Striper/>
+          <Text/>
         </div>
     );
 };
